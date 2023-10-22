3c58e3b924ae60315a5f3dcae0297fa0edffc2c4
Vladislav Bykovskiy <vladislav.bykovskiy@gmail.com>
2023-10-22 12:09:56 +0300

    dev

--- data-sources/filesystem/filesystem.ts
+++ data-sources/filesystem/filesystem.ts
@@ -3,6 +3,8 @@ import SQLite from "sqlite3";
 import { Sequelize } from "sequelize";
 
 export const filesystemSequelize = new Sequelize("blog", "dev", "dev", {
+  logging: false,
+
   dialect: 'sqlite',
   storage: path.resolve(__dirname, "..", "..", "db.sqlite3"),
   dialectOptions: {
--- data-sources/postgres/postgres.ts
+++ data-sources/postgres/postgres.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize";
 
 export const postgresSequelize = new Sequelize("blog", "postgres", "postgres", {
+    logging: false,
+
     dialect: 'postgres',
     host: 'localhost',
     port: 5432,
